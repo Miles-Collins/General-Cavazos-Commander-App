c570cb3ad7ca3c871018a54bc62cf6248cd21f9f
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2026-03-26 21:05:03 -0700

    git-init

---
+++ cavazos/src/main/java/com/cavazos/JSONFile.java
@@ -0,0 +1,35 @@
+package com.cavazos;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public final class JSONFile {
+
+    private JSONFile() {
+        // Utility class
+    }
+
+    // read a json array file from classpath resources
+    public static JSONArray readArray(String resourceName) {
+        JSONParser jsonParser = new JSONParser();
+
+        try (InputStream input = JSONFile.class.getClassLoader().getResourceAsStream(resourceName)) {
+            if (input == null) {
+                return null;
+            }
+            try (Reader reader = new InputStreamReader(input)) {
+                Object obj = jsonParser.parse(reader);
+                return (JSONArray) obj;
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
